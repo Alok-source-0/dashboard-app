6169521a3c2c8a57b570e044e307058c48fdce55
Alok Raj <ralok3303@gmail.com>
2026-01-19 15:37:56 +0530

    Fix API URL for production

--- src/app/dashboard.service.ts
+++ src/app/dashboard.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = '';
 
   constructor(private http: HttpClient) {}
 
